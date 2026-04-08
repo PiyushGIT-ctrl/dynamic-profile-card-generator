df0480509c88241bf54cef20fe1a5da6634aaed1
Piyush <sharmapiyushkv@gmail.com>
2026-04-08 13:27:37 +0530

    Add files via upload

---
+++ jslearn.js
@@ -0,0 +1,49 @@
+let form = document.querySelector("form");
+let input = document.querySelectorAll("input");
+let main = document.querySelector("#main");
+
+
+form.addEventListener("submit", function(details){
+    details.preventDefault();
+
+    console.log(
+        input[0].value,
+        input[1].value,
+        input[2].value,
+        input[3].value
+    )
+
+    //creating a div with id card so that we can have dynamic cards!
+    let card =  document.createElement("div");
+    card.classList.add("card");
+
+    let profile = document.createElement("div");
+    profile.classList.add("profile");
+
+    let img = document.createElement("img");
+    img.setAttribute(
+        "src",
+        input[0].value
+    )
+     //now making h3, h5 and p 
+    let h3 = document.createElement("h3");
+    h3.textContent = input[1].value;
+    let h5 = document.createElement("h5");
+    h5.textContent = input[2].value;
+    let p = document.createElement("p");
+    p.textContent = input[3].value;
+
+    profile.appendChild(img);
+    card.appendChild(profile);
+    card.appendChild(h3);
+    card.appendChild(h5);
+    card.appendChild(p);
+
+    main.appendChild(card);
+
+    input.forEach(function(inp){
+        if (inp.type !== "submit"){
+            inp.value = "";
+        }
+    });
+});
